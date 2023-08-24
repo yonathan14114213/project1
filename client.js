b3e9c0825f13843d7f449c1fe2cdd10ecba8a058
yonathan <yonathan.eisenberg@gmail.com>
2023-08-24 16:14:09 +0300

    11

--- client.js
+++ client.js
@@ -12,6 +12,7 @@ mybutton.addEventListener('click',()=>{
 async function getData() {
     const resp =  await fetch("http://localhost:3000/");
     const data = await resp.json();
+    console.log(data);
     mydiv.textContent = data;
 
 }
